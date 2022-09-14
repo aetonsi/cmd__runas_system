c131846e615579002854e6dd9c0951092baf1527
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-09-14 10:42:46 +0200

    integrazione

--- runas_system.cmd
+++ runas_system.cmd
@@ -1,1 +1,1 @@
-@elevate "%~dp0.\bin__psexec64\psexec64.exe" -accepteula -nobanner -i -d -h -w "%CD%" -s %*
+@"%~dp0.\elevate\elevate" "%~dp0.\bin__psexec64\psexec64" -accepteula -nobanner -i -h -w "%CD%" -s %*
